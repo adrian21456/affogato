b8094d7a89031345872afe36fbb23f3fe646b133
Adrian Duma-op <46225266+adrian21456@users.noreply.github.com>
2025-12-02 11:29:54 +0800

    enhancement: dropdown api

--- src/BaseService.php
+++ src/BaseService.php
@@ -26,13 +26,41 @@ class BaseService
 
     public static function saveFile($file, $filename)
     {
-        if(!$file){
+        if (!$file) {
             return null;
         }
-        
+
         $disk = env('FILESYSTEM_DISK', 'public');
         $path = ($disk === 'gcs') ? env('GOOGLE_CLOUD_STORAGE_FOLDER', 'files') : 'files';
 
         return $file->storeAs($path, $filename, $disk);
+    }
+
+    public function getConfig($config)
+    {
+        if(file_exists(base_path('core/' . $config . '.json')) === false){
+            return json_decode(file_get_contents(base_path('core/' . $config . '.json')), true);
+        }
+        
+        throw new \Exception("Configuration file not found");
+    }
+
+    public static function getDropdownItems($Request, $model)
+    {
+        $config = getConfig($model->table);
+        $value = $Request->input('value', $model->table . "_id");
+        $label = $Request->input('label', $config->representative_value);
+
+        $items = $model::query([$value, $label])->get();
+        $dropdownItems = [];
+
+        foreach ($items as $item) {
+            $dropdownItems[] = [
+                'value' => $item->$value,
+                'label' => $item->$label,
+            ];
+        }
+
+        return $dropdownItems;
     }
 }
